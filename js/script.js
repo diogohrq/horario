db2132eaa22ee528efa98a1fb7ded2bd34719428
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-02-26 11:51:37 -0300

    Fix JS

--- js/script.js
+++ js/script.js
@@ -1,25 +1,50 @@
 const supabaseUrl = "https://tiblnsckvqrmsgfcskko.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpYmxuc2NrdnFybXNnZmNza2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4OTA5MTUsImV4cCI6MjA4NzQ2NjkxNX0.lIUMV88wGiP34bdjhMhm5mO68U6okQODsGlzKnC9xuw";
-
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpYmxuc2NrdnFybXNnZmNza2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4OTA5MTUsImV4cCI6MjA4NzQ2NjkxNX0.lIUMV88wGiP34bdjhMhm5mO68U6okQODsGlzKnC9xuw";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
-const table = document.getElementsByClassName("tb");
-
-function populateTable(table, data) {}
-
-async function getData() {
-  const { data, error } = await supabaseClient.from("horario").select("*");
+async function fetchData() {
+  let { data, error } = await supabaseClient.from("horario").select("*");
 
   if (error) {
     console.error("Error fetching data: ", error);
     return;
   }
 
-  data.forEach((element) => {
-    console.log(element);
-    console.log(table);
-  });
+  // Remove id
+  data = data.map(({id, ...rest }) => rest)
+  return data;
 }
 
-getData();
+async function populateTable() {
+  const data = await fetchData();
+  const tableHeader = document.querySelector('#tbheader')
+  const tableBody = document.querySelector("#tbbody")
+
+  for (let i = 0; i <= data.length - 1; i++) {
+    const headerRow = document.createElement('tr');
+    const bodyRow = document.createElement('tr')
+    const headers = Object.keys(data[0])
+
+    // Table Header
+    if (i == 0) {
+      headers.forEach(element => {
+        const th = document.createElement('th');
+        th.textContent = element;
+        headerRow.appendChild(th);
+      });
+
+      tableHeader.appendChild(headerRow)
+    }
+
+    dataRow = Object.values(data[i])
+    dataRow.forEach(item => {
+      const td = document.createElement('td')
+      td.textContent = item
+      bodyRow.appendChild(td)
+    })
+
+    tableBody.appendChild(bodyRow)
+  }
+}
+
+populateTable()
